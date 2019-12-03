9ba610b87aaa37ad7c6acea6f4cecfb99d57ca90
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-12-03 16:53:27 +0000

    Extract stepping into function

--- day-03.js
+++ day-03.js
@@ -1,45 +1,40 @@
 // Make two arrays, one for each set of positions occupied by the wires
 
+function step(direction, currentPosition) {
+  const steps = {
+    R: {
+      x: currentPosition.x + 1,
+      y: currentPosition.y
+    },
+    U: {
+      x: currentPosition.x,
+      y: currentPosition.y + 1
+    },
+    L: {
+      x: currentPosition.x - 1,
+      y: currentPosition.y
+    },
+    D: {
+      x: currentPosition.x,
+      y: currentPosition.y - 1
+    }
+  };
+
+  return steps[direction];
+}
+
 function makeWirePositions(instructions) {
   let currentPosition = { x: 0, y: 0 };
-  // Don't include origin in history or it will always be closest to origin!
+  // Don't include origin in positions or it will always be closest to origin!
   let positions = [];
+
   instructions.forEach(instruction => {
     const direction = instruction.slice(0, 1);
     const distance = instruction.slice(1);
     for (let i = 0; i < distance; i++) {
-      if (direction === "R") {
-        const newPosition = {
-          x: currentPosition.x + 1,
-          y: currentPosition.y
-        };
-        positions.push(newPosition);
-        currentPosition = newPosition;
-      }
-      if (direction === "U") {
-        const newPosition = {
-          x: currentPosition.x,
-          y: currentPosition.y + 1
-        };
-        positions.push(newPosition);
-        currentPosition = newPosition;
-      }
-      if (direction === "L") {
-        const newPosition = {
-          x: currentPosition.x - 1,
-          y: currentPosition.y
-        };
-        positions.push(newPosition);
-        currentPosition = newPosition;
-      }
-      if (direction === "D") {
-        const newPosition = {
-          x: currentPosition.x,
-          y: currentPosition.y - 1
-        };
-        positions.push(newPosition);
-        currentPosition = newPosition;
-      }
+      const newPosition = step(direction, currentPosition);
+      positions.push(newPosition);
+      currentPosition = newPosition;
     }
   });
   return positions;

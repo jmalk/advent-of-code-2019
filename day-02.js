717b97b2040151839e4364c961cc12b17435fe2d
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-12-05 17:14:12 +0000

    Move intcode computer to lib for easy sharing

--- day-02.js
+++ day-02.js
@@ -1,22 +1,4 @@
-function processIntcode(program) {
-  for (let i = 0; i < program.length; i += 4) {
-    switch (program[i]) {
-      case 99:
-        return program;
-      case 1:
-        program[program[i + 3]] =
-          program[program[i + 1]] + program[program[i + 2]];
-        break;
-      case 2:
-        program[program[i + 3]] =
-          program[program[i + 1]] * program[program[i + 2]];
-        break;
-      default:
-        throw new Error(`Unknown instruction at ${i}: ${program[i]}`);
-    }
-  }
-  return program;
-}
+const { processIntcode } = require("./lib/intcode-computer");
 
 const readFileSync = require("./lib/read-file-sync");
 
@@ -43,5 +25,3 @@ for (let noun = 0; noun < 100; noun++) {
     }
   }
 }
-
-module.exports = { processIntcode };
